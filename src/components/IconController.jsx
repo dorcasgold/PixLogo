fef6c717fe4cf3adce35966c12fc8d3ae18fafa2
odetayo dorcas <odetayodorcas123@gmail.com>
2024-10-02 09:23:29 +0100

    feat: implements controllers

--- src/components/IconController.jsx
+++ src/components/IconController.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import { Smile } from "lucide-react"
+import { Slider } from "./ui/slider"
+import { useEffect, useState } from "react"
+import ColorPickerController from "./ColorPickerController";
 
 function IconController() {
+  const [size, setSize] = useState(280);
+  const [rotate, setRotate] = useState(0);
+  const [color, setColor] = useState("#fff");
+  const storageValue = JSON.parse(localStorage.getItem('value'));
+
+  useEffect(() => {
+    const updatedValue = {
+      ...storageValue,
+      iconSize: size,
+      iconRotate: rotate,
+      iconColor: color,
+      icon: "Smile"
+    }
+    localStorage.setItem('value', JSON.stringify(updatedValue))
+  }, [size, rotate, color])
+
   return (
-    <div>IconController</div>
+    <div className="text-slate-50">
+      <div>
+        <label>Icon</label>
+        <div className="p-3 cursor-pointer text-slate-900 bg-gray-200 rounded-md w-[50px] h-[50px] my-2 flex items-center justify-center">
+          <Smile />
+        </div>
+        <div className=" py-2">
+          <label className="p-2 flex justify-between items-center">Size <span>{size} px</span></label>
+          <Slider defaultValue={[280]} max={512} step={1}
+            onValueChange={(event) => setSize(event[0])}
+          />
+        </div>
+        <div className=" py-2">
+          <label className="p-2 flex justify-between items-center">Rotate <span>{rotate} °</span></label>
+          <Slider defaultValue={[0]} max={360} step={1}
+            onValueChange={(event) => setRotate(event[0])}
+          />
+        </div>
+        <div className=" py-2">
+          <label className="p-2 flex justify-between items-center">Icon Color</label>
+          <ColorPickerController hideController={true}
+            selectedColor={(color) => setColor(color)} />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -9,19 +9,16 @@ function Mainpage() {
 
   return (
     <main className="font-sans bg-slate-950 text-slate-100 h-screen overflow-hidden">
-      {/* Header */}
       <Header />
 
-      {/* Flex container for Sidebar and Main content */}
       <div className="flex h-full">
 
-        {/* Sidebar (on small screens, icons only) */}
         <div className="w-16 md:w-64 h-full flex-shrink-0">
           <SideNav selectedIndex={(value) => setSelectedIndex(value)} />
         </div>
 
         {/* Main content area */}
-        <div className="flex-grow grid grid-cols-1 md:grid-cols-2 gap-4 overflow-auto">
+        <div className="flex-grow grid grid-cols-1 md:grid-cols-2 gap-1 overflow-auto">
           {/* First content block */}
           <div className="border shadow-sm p-5">
             {selectedIndex === 0 ? <IconController /> : <BackgroundController />}
